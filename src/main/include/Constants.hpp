f4f22f7ab2ccbc768fde9c3f9e154137ac42ab09
Jeron Carleton <jeshgrca@gmail.com>
2024-11-26 21:24:10 -0500

    fix Zac's shit ass formatting, compilation errors
    Signed-off-by: Jeron Carleton <jeshgrca@gmail.com>

--- src/main/include/Constants.hpp
+++ src/main/include/Constants.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
+#include <ctre/phoenix6/Pigeon2.hpp>
 #include <ctre/phoenix6/swerve/SwerveDrivetrainConstants.hpp>
 #include <ctre/phoenix6/swerve/SwerveModuleConstants.hpp>
-#include <ctre/phoenix6/swerve/utility/Geometry.hpp>
-#include <ctre/phoenix6/swerve/utility/Kinematics.hpp>
-#include <ctre/phoenix6/Pigeon2.hpp>
 #include <frc/kinematics/SwerveDriveKinematics.h>
-#include <frc/kinematics/SwerveDriveOdometry3d.h>
-#include <frc/kinematics/SwerveDriveOdometry.h>
-#include <frc/geometry/Pose2d.h>
-#include <frc/geometry/Pose3d.h>
- 
 
 namespace constants {
 constexpr char canBus[] = "rio";
@@ -36,11 +29,9 @@ constexpr ctre::phoenix6::swerve::SwerveModuleConstants rearRightModule =
 constexpr ctre::phoenix6::swerve::SwerveDrivetrainConstants
     drivetrainConstants{};
 }; // namespace swerve
-namespace odometry{
-    ctre::phoenix6::hardware::Pigeon2 m_gyro{9};
-    frc::SwerveDriveKinematics<4> m_kinematics;
-    
 
-
-}; // namespace odemetry
+namespace odometry {
+// ctre::phoenix6::hardware::Pigeon2 gyro{9};
+// frc::SwerveDriveKinematics<4> m_kinematics;
+}; // namespace odometry
 }; // namespace constants
