defcec850f72255fd2766c1cf5d481df78a8e2b2
Jeron Carleton <jeshgrca@gmail.com>
2024-12-01 16:14:46 -0500

    remove duplicate method call in TeleopInit
    Signed-off-by: Jeron Carleton <jeshgrca@gmail.com>

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -10,11 +10,4 @@ void RobotContainer::TeleopInit() {
             driverController.GetRightX(), driverController.GetRightY());
       },
       {&driveSubsystem}));
-  driveSubsystem.SetDefaultCommand(frc2::cmd::Run(
-      [this] {
-        driveSubsystem.Drive(
-            driverController.GetLeftX(), driverController.GetLeftY(),
-            driverController.GetRightX(), driverController.GetRightY());
-      },
-      {&driveSubsystem}));
 }
