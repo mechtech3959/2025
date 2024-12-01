97c35cdbc77b57b332a3dd1b7a054369e82f5426
Jeron Carleton <jeshgrca@gmail.com>
2024-12-01 16:10:24 -0500

    fix formatting and formatting action
    Signed-off-by: Jeron Carleton <jeshgrca@gmail.com>

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -2,21 +2,19 @@
 
 #include "RobotContainer.hpp"
 
-void RobotContainer::TeleopInit()
-{
+void RobotContainer::TeleopInit() {
   driveSubsystem.SetDefaultCommand(frc2::cmd::Run(
-      [this]
-      {
+      [this] {
         driveSubsystem.Drive(
             driverController.GetLeftX(), driverController.GetLeftY(),
             driverController.GetRightX(), driverController.GetRightY());
       },
       {&driveSubsystem}));
   driveSubsystem.SetDefaultCommand(frc2::cmd::Run(
-      [this]
-      {
-         driveSubsystem.Drive(
-          driverController.GetLeftX(), driverController.GetLeftY(),
-          driverController.GetRightX(), driverController.GetRightY()); },
+      [this] {
+        driveSubsystem.Drive(
+            driverController.GetLeftX(), driverController.GetLeftY(),
+            driverController.GetRightX(), driverController.GetRightY());
+      },
       {&driveSubsystem}));
 }
