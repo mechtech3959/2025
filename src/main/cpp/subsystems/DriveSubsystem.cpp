4241d8f3a01d64f309a21ade44195ad8961b41fb
Jeron Carleton <jeshgrca@gmail.com>
2024-12-04 09:57:00 -0500

    whitespace
    Signed-off-by: Jeron Carleton <jeshgrca@gmail.com>

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -1,6 +1,7 @@
-#include "subsystems/DriveSubsystem.hpp"
 #include <ctre/phoenix6/swerve/SwerveModule.hpp>
 #include <ctre/phoenix6/swerve/SwerveRequest.hpp>
+
+#include "subsystems/DriveSubsystem.hpp"
 
 void DriveSubsystem::Drive(double x1, double y1, double x2, double y2) {
   Drivetrain.SetControl(ctre::phoenix6::swerve::requests::RobotCentric{}
@@ -8,6 +9,7 @@ void DriveSubsystem::Drive(double x1, double y1, double x2, double y2) {
                             .WithVelocityY(y1 * 1_m / 1_s)
                             .WithRotationalRate(x2 * 1_tr / 1_s));
 };
+
 void DriveSubsystem::DriveFOC(double x1, double y1, double x2, double y2,
                               frc::Rotation2d operatorAngle) {
   Drivetrain.SetControl(
@@ -18,9 +20,11 @@ void DriveSubsystem::DriveFOC(double x1, double y1, double x2, double y2,
           .WithForwardPerspective(ctre::phoenix6::swerve::requests::
                                       ForwardPerspectiveValue::BlueAlliance));
 };
+
 void DriveSubsystem::startOdm() { Drivetrain.GetOdometryThread().Start(); };
+
 void DriveSubsystem::State() {
-  frc::Rotation3d m_rot = Drivetrain.GetRotation3d();
-  frc::Pose2d m_pose = Drivetrain.GetState().Pose;
-  frc::ChassisSpeeds m_speed = Drivetrain.GetState().Speeds;
+  frc::Rotation3d rot = Drivetrain.GetRotation3d();
+  frc::Pose2d pose = Drivetrain.GetState().Pose;
+  frc::ChassisSpeeds speed = Drivetrain.GetState().Speeds;
 };
