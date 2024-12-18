45d1389d388a12abce42ce85b6b08e0eaa156b3f
Zachary Taylor <zachary@localhost.localdomain>
2024-12-18 12:29:35 -0600

    nasty function to change current limits

--- src/main/include/subsystems/DriveSubsystem.hpp
+++ src/main/include/subsystems/DriveSubsystem.hpp
@@ -5,6 +5,7 @@
 #include <frc2/command/SubsystemBase.h>
 
 #include "Constants.hpp"
+#include "ctre/phoenix6/configs/Configs.hpp"
 
 class DriveSubsystem : public frc2::SubsystemBase {
 public:
@@ -12,6 +13,17 @@ public:
 
   void SetControl(ctre::phoenix6::swerve::requests::SwerveRequest &&request) {
     drivetrain.SetControl(request);
+  };
+  // function to dynamically change current limits
+  void
+  setCurrentLimits(ctre::phoenix6::configs::CurrentLimitsConfigs driveLimit,
+                   ctre::phoenix6::configs::CurrentLimitsConfigs turnLimit) {
+    for (int i = 0; i < 4; ++i) {
+      drivetrain.GetModule(i).GetDriveMotor().GetConfigurator().Apply(
+          driveLimit);
+      drivetrain.GetModule(i).GetSteerMotor().GetConfigurator().Apply(
+          turnLimit);
+    }
   };
   // prepares requests for drive control mode switch
 
