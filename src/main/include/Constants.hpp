ed0bb800363535118fabaa3be3e178464f3d8cce
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-01-15 22:10:53 -0600

    reconfig modules and max speed

--- src/main/include/Constants.hpp
+++ src/main/include/Constants.hpp
@@ -20,6 +20,10 @@ namespace swerve {
 constexpr double translationGain = 5.0;
 constexpr double rotationGain = 1.5;
 static constexpr units::scalar_t coupleRatio = 3.5714285714285716;
+static constexpr units::turn_t FrontLeftEncoderOffset = 0.2080078125_tr;
+static constexpr units::turn_t FrontRightEncoderOffset = -0.138916015625_tr;
+static constexpr units::turn_t BackLeftEncoderOffset = -0.048583984375_tr;
+static constexpr units::turn_t BackRightEncoderOffset = 0.2001953125_tr;
 
 // puts a limit on the current draw to avoid brownout
 constexpr ctre::phoenix6::configs::CurrentLimitsConfigs driveCurrentLimit =
@@ -67,7 +71,7 @@ constexpr ctre::phoenix6::swerve::SwerveModuleConstantsFactory moduleCreator =
                                  .WithKV(0.124))
         .WithSteerMotorGains(
             ctre::phoenix6::configs::Slot0Configs{}
-                .WithKP(100)
+                .WithKP(7)
                 .WithKI(0)
                 .WithKD(0.5)
                 .WithKS(0.1)
@@ -89,24 +93,27 @@ constexpr ctre::phoenix6::swerve::SwerveModuleConstantsFactory moduleCreator =
 // point 16x13.5 inches METERS: 0.8128x0.6858  0.4064x0.3429 0.4064x0.3429 is
 // considered th origin (0,0)
 constexpr ctre::phoenix6::swerve::SwerveModuleConstants frontLeftModule =
-    moduleCreator.CreateModuleConstants(7, 8, 12, 77.8_deg, -0.6064_m, 0.3429_m,
-                                        false, false, false);
+    moduleCreator.CreateModuleConstants(7, 8, 12, FrontLeftEncoderOffset,
+                                        -0.6064_m, 0.3429_m, false, false,
+                                        false);
 constexpr ctre::phoenix6::swerve::SwerveModuleConstants frontRightModule =
-    moduleCreator.CreateModuleConstants(1, 3, 13, -47.9_deg, 0.6064_m, 0.3429_m,
-                                        true, false, false);
+    moduleCreator.CreateModuleConstants(1, 3, 13, FrontRightEncoderOffset,
+                                        0.6064_m, 0.3429_m, true, false, false);
 constexpr ctre::phoenix6::swerve::SwerveModuleConstants rearLeftModule =
-    moduleCreator.CreateModuleConstants(5, 6, 14, -17.45_deg, -0.6064_m,
-                                        -0.3429_m, false, false, false);
+    moduleCreator.CreateModuleConstants(5, 6, 14, BackLeftEncoderOffset,
+                                        -0.6064_m, -0.3429_m, false, false,
+                                        false);
 constexpr ctre::phoenix6::swerve::SwerveModuleConstants rearRightModule =
-    moduleCreator.CreateModuleConstants(2, 4, 15, 72.06_deg, 0.6064_m,
-                                        -0.3429_m, true, false, false);
+    moduleCreator.CreateModuleConstants(2, 4, 15, BackRightEncoderOffset,
+                                        0.6064_m, -0.3429_m, true, false,
+                                        false);
 constexpr ctre::phoenix6::swerve::SwerveDrivetrainConstants
     drivetrainConstants =
         ctre::phoenix6::swerve::SwerveDrivetrainConstants{}.WithPigeon2Id(
             pigeonID);
 //! set to max constraints of swerve modules!
-constexpr double maxDriveSpeedMPS = 5.01;
-constexpr double maxFOCDriveSpeedMPS = 4.78;
+constexpr units::meters_per_second maxDriveSpeedMPS = 5.01_mps;
+constexpr units::meters_per_second maxFOCDriveSpeedMPS = 4.78_mps;
 }; // namespace swerve
 
 // namespace odometry {
