382644edb52b1afed9ba78cf0aa9395045708693
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-01-24 12:28:48 -0600

    tag map

--- src/main/cpp/subsystems/LimeLightSubsystem.cpp
+++ src/main/cpp/subsystems/LimeLightSubsystem.cpp
@@ -5,10 +5,10 @@ LimeLight::LimeLight(std::string name) {
   limelight = nt::NetworkTableInstance::GetDefault().GetTable(name);
 };
 void LimeLight::updateTracking(std::string name) {
-   tx = LimelightHelpers::getTX(name);
-   ty = LimelightHelpers::getTY(name);
-   ta = LimelightHelpers::getTA(name);
-   tv = LimelightHelpers::getTV(name);
+  tx = LimelightHelpers::getTX(name);
+  ty = LimelightHelpers::getTY(name);
+  ta = LimelightHelpers::getTA(name);
+  tv = LimelightHelpers::getTV(name);
   if (tv < 1.0) {
     drivecmd = 0.0;
     turncmd = 0.0;
@@ -21,9 +21,12 @@ void LimeLight::updateTracking(std::string name) {
     drivecmd = clamp(ta, -0.5, 0.5);
   }
 };
-frc::Pose2d LimeLight::poseEst(std ::string name) { if(LLHasTarget && (tx < 0.1 || tx > -0.1)){
-  std::optional<LimelightHelpers::PoseEstimate> posEst = LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2(name);
-  frc::Pose2d p =  (posEst.has_value())?  frc::Pose2d{posEst->pose} :  frc::Pose2d{0_m,0_m,0_deg};
-  return p;
-  
-}  };
+frc::Pose2d LimeLight::poseEst(std ::string name) {
+  if (LLHasTarget && (tx < 0.1 || tx > -0.1)) {
+    std::optional<LimelightHelpers::PoseEstimate> posEst =
+        LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2(name);
+    frc::Pose2d p = (posEst.has_value()) ? frc::Pose2d{posEst->pose}
+                                         : frc::Pose2d{0_m, 0_m, 0_deg};
+    return p;
+  }
+};
