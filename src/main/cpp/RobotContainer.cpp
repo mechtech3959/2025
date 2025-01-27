3335d3d445931dfb9d7352e254e547369c7a8d2e
Zachary Taylor <Zacharytaylor0508@gmail.com>
2025-01-27 13:46:34 -0600

    one more

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -12,7 +12,7 @@ RobotContainer::RobotContainer() {
   limelight.updateTracking();
   visionEstimate = limelight.poseEst();
 }
-void RobotContainer::RobotPeriodic() {};
+
 void RobotContainer::ConfigureBindings() {
   // Note that X is defined as forward according to WPILib convention,
   // and Y is defined as to the left according to WPILib convention.
