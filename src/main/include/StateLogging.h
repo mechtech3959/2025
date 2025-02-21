e128f91173d90c8c29da7b544399e72c814f3def
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-02-21 13:22:53 -0600

    elevator init

---
+++ src/main/include/StateLogging.h
@@ -0,0 +1,14 @@
+#include <ctre/phoenix6/CANcoder.hpp>
+#include <ctre/phoenix6/TalonFX.hpp>
+
+namespace customLogging {
+
+struct ElevatorState {
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderPose;
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderABSPose;
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> masterPose;
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> slavePose;
+  units::inch_t elevatorPose;
+};
+
+} // namespace customLogging
