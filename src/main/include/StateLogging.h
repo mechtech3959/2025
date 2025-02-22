49aaf3c17c9bc4a5de86b5b89d36f1c2fb7373e0
Zachary Taylor <Zacharytaylor0508@gmail.com>
2025-02-21 20:50:43 -0600

    current and target

--- src/main/include/StateLogging.h
+++ src/main/include/StateLogging.h
@@ -9,12 +9,16 @@ struct ElevatorState {
   ctre::phoenix6::StatusSignal<units::angle::turn_t> masterPose;
   ctre::phoenix6::StatusSignal<units::angle::turn_t> slavePose;
   units::inch_t elevatorPose;
+  units::inch_t targetPose;
 };
 struct ClawState {
-    ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderPose;
-    ctre::phoenix6::StatusSignal<units::angle::turn_t> motorPose;
-    units::degree_t currentAngle;
-    units::degree_t lastKnowAngle; 
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderPose;
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderABSPose;
+  ctre::phoenix6::StatusSignal<units::angle::turn_t> motorPose;
+  units::degree_t currentAngle;
+  units::degree_t lastKnowAngle;
+  units::degree_t targetAngle;
+  bool coralDetected;
 };
 
 } // namespace customLogging
