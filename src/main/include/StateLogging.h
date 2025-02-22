c6aeb184a9497aadcd9f15cf75d12aa5ace1c3d3
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-02-22 16:17:59 -0600

    state logging unit fix

--- src/main/include/StateLogging.h
+++ src/main/include/StateLogging.h
@@ -4,17 +4,17 @@
 namespace customLogging {
 
 struct ElevatorState {
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderPose;
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderABSPose;
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> masterPose;
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> slavePose;
+  units::angle::turn_t encoderPose;
+  units::angle::turn_t encoderABSPose;
+  units::angle::turn_t masterPose;
+  units::angle::turn_t slavePose;
   units::inch_t elevatorPose;
   units::inch_t targetPose;
 };
 struct ClawState {
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderPose;
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> encoderABSPose;
-  ctre::phoenix6::StatusSignal<units::angle::turn_t> motorPose;
+  units::angle::turn_t encoderPose;
+  units::angle::turn_t encoderABSPose;
+  units::angle::turn_t motorPose;
   units::degree_t currentAngle;
   units::degree_t lastKnowAngle;
   units::degree_t targetAngle;
