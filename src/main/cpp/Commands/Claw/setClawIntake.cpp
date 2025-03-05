6c32e9e9ba9c8a9dfcf5f4b2fc008f3e3cd36fdd
DEKE21 <Zacharytaylor0508@gmail.com>
2025-03-05 11:36:57 -0600

    intake end at 30_deg

--- src/main/cpp/Commands/Claw/setClawIntake.cpp
+++ src/main/cpp/Commands/Claw/setClawIntake.cpp
@@ -9,7 +9,9 @@ void setClawIntake::Initialize() {
   Claw->setAxis(0_deg);
   Claw->setIntake();
 }
-
+void setClawIntake::End(){
+    Claw->setAxis(30_deg);
+}
 bool setClawIntake::IsFinished() {
   if (Claw->endIntake == true)
     return true;
--- src/main/include/Commands/Claw/setClawIntake.h
+++ src/main/include/Commands/Claw/setClawIntake.h
@@ -16,6 +16,8 @@ public:
 
   // void Execute() override;
 
+  void End() override;
+
   bool IsFinished() override;
 
 private:
